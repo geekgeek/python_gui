55bbc0bb017ff073092249211ba95b1daf22bd43
geekgeek <solq87@gmail.com>
2015-07-12 01:01:22 +0200

    Create gui_file_1.py

---
+++ gui_file_1.py
@@ -0,0 +1,13 @@
+#CREATE GUI WITH PYTHON
+
+from Tkinter import *
+#blank window
+root = Tk()
+
+#label
+TheLabel = Label(root, text='this is easy')
+TheLabel.pack()
+
+#loop / show window
+root.mainloop()
+
