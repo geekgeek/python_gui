33013e8f780e613f0988f2e1afa4375709bf09f0
geekgeek <solq87@gmail.com>
2015-07-23 07:34:18 +0200

    Create checkb_button.py

---
+++ checkb_button.py
@@ -0,0 +1,30 @@
+
+from Tkinter import *
+
+
+#blank window
+root = Tk()
+
+#create label,textboxes and checkbox
+label1 = Label(root, text="lb1")
+label2 = Label(root, text="label2")
+
+textbox1 = Entry(root)
+textbox2 = Entry(root)
+
+def printName():
+    print "hello this is function"
+
+checkbox1 = Checkbutton(root, text="checbox1")
+
+button1 = Button(root, text="call function", command=printName)
+
+#Create alignments / sticky e = align to east
+label1.grid(row=0, sticky=E)
+label2.grid(row=1, sticky=E)
+
+textbox1.grid(row=0, column=1)
+textbox2.grid(row=1, column=1)
+button1.grid(row=2,column=1)
+
+root.mainloop()
